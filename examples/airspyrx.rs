e4dd25757e7c1a003591b1895b41f86731be0e88
Andres Vahter <andres.vahter@gmail.com>
2015-07-18 16:04:47 +0300

    example airspyrx.rs now outputs IQ data to stdout

--- examples/airspyrx.rs
+++ examples/airspyrx.rs
@@ -1,11 +1,17 @@
 
 extern crate airspy;
+use airspy::Airspy;
 use airspy::AirspySamplerate;
+use airspy::{IQ};
 
 #[macro_use]
 extern crate log;
 extern crate fern;
 extern crate time;
+
+use std::io;
+use std::io::Write;
+use std::env;
 
 fn main() {
     // setup fern logger
@@ -23,10 +29,42 @@ fn main() {
         panic!("Failed to initialize global logger: {}", e);
     }
 
-    match airspy::Airspy::new() {
+    // check args
+    let mut args = env::args();
+    let mut frequency_hz = 103_400_000;
+    if args.len() == 1 {
+        info!("use default frequency: {:.3} MHz", frequency_hz as f64/1000_000f64);
+    }
+    else {
+        frequency_hz = (args.nth(1).unwrap().parse::<f32>().unwrap() as f64 * 1000_000f64) as u32;
+        info!("frequency: {:.3} MHz", frequency_hz as f64/1000_000f64);
+    }
+
+    match Airspy::<IQ<i16>>::new() {
         Ok(mut device) => {
             info!("device opened sucessfully");
-            device.set_samplerate(AirspySamplerate::Sps10000000).unwrap();
+            device.set_samplerate(AirspySamplerate::Sps2500000).unwrap();
+
+            device.set_vga_gain(6).unwrap();
+            device.set_mixer_gain(5).unwrap();
+            device.set_lna_gain(7).unwrap();
+
+            device.set_frequency(frequency_hz).unwrap();
+            device.start_rx().unwrap();
+
+            let mut stdout = io::stdout();
+
+            loop {
+                // actually iterator blocks here
+                for s in device.samples() {
+                    let sliceu8 = Airspy::convert_samples_to_bytes(&s);
+
+                    stdout.write(sliceu8).unwrap();
+                    stdout.flush().unwrap();
+                }
+
+                info!("sleep");
+            }
         }
         Err(e) => {
             error!("error {:?}", e);
